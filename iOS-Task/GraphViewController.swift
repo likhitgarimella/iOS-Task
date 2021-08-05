0a5b35e5361045a5077dbf688d93e2206645a501
Likhit Garimella <likhitgmella@gmail.com>
2021-08-05 09:43:52 +0530

    2 charts code

--- iOS-Task/GraphViewController.swift
+++ iOS-Task/GraphViewController.swift
@@ -13,16 +13,16 @@ class GraphViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let chartConfig = ChartConfigXY(
-            xAxisConfig: ChartAxisConfig(from: 2, to: 14, by: 2),
-            yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 2)
+        let chartConfig1 = ChartConfigXY(
+            xAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 1),
+            yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 1)
         )
         
-        let frame = CGRect(x: 40, y: 0, width: 300, height: 500)
+        let frame1 = CGRect(x: 40, y: 0, width: 300, height: 500)
         
-        let chart = LineChart(
-            frame: frame,
-            chartConfig: chartConfig,
+        let chart1 = LineChart(
+            frame: frame1,
+            chartConfig: chartConfig1,
             xTitle: "X axis",
             yTitle: "Y axis",
             lines: [
@@ -31,8 +31,29 @@ class GraphViewController: UIViewController {
             ]
         )
         
-        self.view.addSubview(chart.view)
+        self.view.addSubview(chart1.view)
+        
+        ///
+        
+        let chartConfig2 = ChartConfigXY(
+            xAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 1),
+            yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 1)
+        )
+        
+        let frame2 = CGRect(x: 40, y: 100, width: 300, height: 500)
+        
+        let chart2 = LineChart(
+            frame: frame2,
+            chartConfig: chartConfig2,
+            xTitle: "X axis",
+            yTitle: "Y axis",
+            lines: [
+                (chartPoints: [(100.0, 400.0), (300.0, 300.0), (500.0, 500.0), (600.0, 400.0), (700.0, 400.0), (800.0, 200.0), (1000.0, 300.0)], color: UIColor.blue)
+            ]
+        )
+        
+        self.view.addSubview(chart2.view)
         
     }
     
-}   // #39
+}   // #60
