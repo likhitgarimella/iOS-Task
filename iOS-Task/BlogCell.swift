723fc6243e744c07992151ea43fcb76317d7d0ce
Likhit Garimella <likhitgmella@gmail.com>
2021-08-05 10:57:32 +0530

    blogs table view

---
+++ iOS-Task/BlogCell.swift
@@ -0,0 +1,22 @@
+//
+//  BlogCell.swift
+//  iOS-Task
+//
+//  Created by Likhit Garimella on 05/08/21.
+//
+
+import UIKit
+
+class BlogCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}   // #23
--- iOS-Task/BlogsViewController.swift
+++ iOS-Task/BlogsViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class BlogsViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var tableView: UITableView!
+    
+    var items = [UIImage(named: "one"), UIImage(named: "two")]
+    
+    func Properties() {
         
-        
+        tableView.backgroundColor = UIColor.white
+        tableView.estimatedRowHeight = 140
+        tableView.rowHeight = UITableView.automaticDimension
         
     }
     
-}   // #20
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Properties()
+        
+    }
+    
+}
+
+extension BlogsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BlogCell", for: indexPath) as! BlogCell
+        cell.backgroundColor = UIColor.white
+        return cell
+    }
+    
+}   // #46
