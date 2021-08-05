1f81543f5b14120af6e7d29a754b036c6112f728
Likhit Garimella <likhitgmella@gmail.com>
2021-08-05 09:19:44 +0530

    line chart

--- iOS-Task/GraphViewController.swift
+++ iOS-Task/GraphViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import SwiftCharts
 
 class GraphViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let chartConfig = ChartConfigXY(
+            xAxisConfig: ChartAxisConfig(from: 2, to: 14, by: 2),
+            yAxisConfig: ChartAxisConfig(from: 0, to: 14, by: 2)
+        )
         
+        let frame = CGRect(x: 40, y: 0, width: 300, height: 500)
+        
+        let chart = LineChart(
+            frame: frame,
+            chartConfig: chartConfig,
+            xTitle: "X axis",
+            yTitle: "Y axis",
+            lines: [
+                (chartPoints: [(1.0, 4.0), (5.0, 6.0)], color: UIColor.red),
+                (chartPoints: [(6.0, 5.0), (7.0, 4.0), (9.0, 3.6), (11.0, 3.8), (13.0, 3.4)], color: UIColor.blue)
+            ]
+        )
+        
+        self.view.addSubview(chart.view)
         
     }
     
-}   // #20
+}   // #39
